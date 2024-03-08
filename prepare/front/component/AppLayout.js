51777f400640bd67165320c60951edcfc5c7c195
youngbiny <ultrabab@idstrust.com>
2024-03-08 16:11:29 +0900

    menu

--- prepare/front/component/AppLayout.js
+++ prepare/front/component/AppLayout.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
 
+const items = [
+  {
+    label: (
+      <a href="/" target="_self" rel="noopener noreferrer">
+        노드버드
+      </a>
+    ),
+    key: 'home',
+  },
+  {
+    label: (
+      <a href="/profile" target="_self" rel="noopener noreferrer">
+        Profile
+      </a>
+    ),
+    key: 'app',
+  },
+  {
+    label: (
+      <a href="/signup" target="_self" rel="noopener noreferrer">
+        signup
+      </a>
+    ),
+    key: 'alipay',
+  },
+];
 
 const AppLayout = ({children}) => {
+  const [current, setCurrent] = useState('home');
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
   return(
     <div>
-      <div>공통메뉴</div>
+      <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
       {children}
     </div>
+    // <div>
+    //   <div>
+    //     <Link href="/"><a>노드버드</a></Link>
+    //     <Link href="/profile"><a>프로필</a></Link>
+    //     <Link href="/signup"><a>회원가입</a></Link>
+    //   </div>
+    //   {children}
+    // </div>
   );
 }
 
